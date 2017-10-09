ef89260372c6df7dd9c6f83ddd6b0d0cc394689c
Joe Zyirek <jzyirek@gmail.com>
2017-10-09 16:32:56 -0700

    Initial commit

---
+++ app/src/main/java/com/admin/classportal/ClassPortalActivity.java
@@ -0,0 +1,15 @@
+package com.admin.classportal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassPortalActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_portal);
+    }
+}
